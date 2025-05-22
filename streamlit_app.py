c2ce46312ab2a23e660944c73b9fa612e42a2bc0
Rinyzz <arinytsabita@gmail.com>
2025-05-22 14:47:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.image("9a24d48eafa6ba090c13cb91bcda5323-6462152e08a8b53812152342.jpg")
 st.image("39568f388d8fb70c33681c332fc9060b.jpg")
 
 st.title("Bingung sumpah")
-st.Header("Ngitung apa y?")
+st.header("Ngitung apa y?")
 
 angka = st.number_input("Ngitungngitung:", value=0, step=1)
 
