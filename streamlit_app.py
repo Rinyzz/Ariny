ab5f1f67260e4f210f14d71bd7157ab321d257c9
Rinyzz <arinytsabita@gmail.com>
2025-05-22 14:42:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,13 @@ st.write(
 
 st.image("9a24d48eafa6ba090c13cb91bcda5323-6462152e08a8b53812152342.jpg")
 st.image("39568f388d8fb70c33681c332fc9060b.jpg")
+
+st.title("Bingung sumpah")
+st.Header("Ngitung apa y?")
+angka = st.numper_input("Ngitungngitung:", value=0, strp=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan Genap")
+
+else :
+    st.write(f"{angka} adalah Bilangan Ganjil")
